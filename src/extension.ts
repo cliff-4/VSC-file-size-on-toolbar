59ee5098d0e8a5770e589448e5fd45a12f07b5f1
cliff-4 <adityamishra0700@gmail.com>
2024-11-08 05:17:44 +0530

    feat: text file size is displayed on focus

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,57 @@
 import * as vscode from "vscode";
+import fs from "fs/promises";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
+
+async function getFileSize(path: string): Promise<number> {
+    try {
+        const stats = await fs.stat(path);
+        return stats.size;
+    } catch (e) {
+        console.log("Error getting file size:", e);
+    }
+    return 0;
+}
+
+async function getSize(path: string): Promise<string> {
+    let s: number = await getFileSize(path);
+    let x = 1024;
+    if (s < x) {
+        return (Math.round(100 * s) / 100).toString() + " B";
+    }
+    s /= x;
+    if (s < x) {
+        return (Math.round(100 * s) / 100).toString() + " KB";
+    }
+    s /= x;
+    if (s < x) {
+        return (Math.round(100 * s) / 100).toString() + " MB";
+    }
+    s /= x;
+    if (s < x) {
+        return (Math.round(100 * s) / 100).toString() + " GB";
+    }
+    s /= x;
+    return "N/A B";
+}
+
 export function activate(context: vscode.ExtensionContext) {
     vscode.window.showInformationMessage(
         '"file-size-on-toolbar" is now active'
     );
+
+    let subObj = vscode.window.onDidChangeActiveTextEditor(async (editor) => {
+        if (editor === undefined) {
+            return;
+        }
+        // vscode.window.showInformationMessage(
+        //     `Focused file: ${editor.document.fileName}`
+        // );
+        let activeDoc = editor.document;
+        let sizeStr = await getSize(activeDoc.fileName);
+        vscode.window.showInformationMessage(sizeStr);
+    });
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
@@ -19,7 +65,7 @@ export function activate(context: vscode.ExtensionContext) {
         }
     );
 
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable, subObj);
 }
 
 // This method is called when your extension is deactivated
